9bb546a229224f7f13d990517039428f820622ff
Vikoleonidas <98889491+Vikoleonidas@users.noreply.github.com>
2022-02-02 22:21:09 +0700

    Add files via upload

--- shop/grupbot.js
+++ shop/grupbot.js
@@ -3,13 +3,13 @@ const gcbotwa = () =>{
 	return`Join Aja Semua Fitur Bot Bisa Digunakan !
 
 1. *Alphabot Support*
-_https://chat.whatsapp.com/EU890BcXjyBDkNaUT5WmYV_
+_https://chat.whatsapp.com/GsuP6Mw4jjs7qm4XtXkwAd_
 2. *Alphabot Support 2*
-_https://chat.whatsapp.com/E8NExJwIbhBJYzssfqJNsE_
+_https://chat.whatsapp.com/GsuP6Mw4jjs7qm4XtXkwAd_
 3. *Alphabot Support 3*
-_https://chat.whatsapp.com/KCSqHTky1apG7ApePsfiPy_
+_https://chat.whatsapp.com/GsuP6Mw4jjs7qm4XtXkwAd_
 4. *Alphabot Support 4* 
-_https://chat.whatsapp.com/KwmvHr7VMFj7r5ry9xmMsU_
+_https://chat.whatsapp.com/GsuP6Mw4jjs7qm4XtXkwAd_
 
 Jika ada link yang ke reset, silahkan hubungi
 owner untuk meminta link yang baru
