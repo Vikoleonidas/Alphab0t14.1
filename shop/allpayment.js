55608142454e74492df841aaf7281696723ca1ed
Vikoleonidas <98889491+Vikoleonidas@users.noreply.github.com>
2022-02-02 22:19:47 +0700

    Add files via upload

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -1,31 +1,20 @@
 const allpayment = (prefix) => {
-return `PAYMENT BY ZEEONE OFC
+return `PAYMENT BY Ryutenzo
 
-*🏧 BANK*
-
-1. BRI 
-	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
-	
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0887 4350 47326
+	ㅁ 0838 1908 2780
 2. OVO
-	ㅁ 0887 4350 47326
+	ㅁ 0838 1908 2780
 3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ 0838 1908 2780
 	
-Semuanya atas nama RI****DI
+Semuanya atas nama FE***K*****
 
 *📲 PULSA*
 
-Tidak tersedia
+   ㅁ 0838 1908 2780
 	
 Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
 `
